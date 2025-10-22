9e1aac5da8d4314ea0a2f2a88fa1e09684d51c98
saurav <saurav.mishra0784@gmail.com>
2025-10-22 13:04:55 +0530

    first portfolio

---
+++ script.js
@@ -0,0 +1,11 @@
+// Simple smooth scroll for nav links
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href');
+        document.querySelector(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
